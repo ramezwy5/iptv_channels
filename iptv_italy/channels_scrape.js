897f6fc31b7dad86be3fa9ecd27867ad3cbed2c4
ramezwy5 <ramez4fx@gmail.com>
2026-02-24 20:21:17 -0500

    italy channels

--- iptv_italy/channels_scrape.js
+++ iptv_italy/channels_scrape.js
@@ -1,10 +1,20 @@
 import fs from 'node:fs';
 import puppeteer from 'puppeteer';
 
-
+function showsJson(path, list){
+  const fixError = (error)=>{
+    if(error){
+      console.error(error)
+      return;
+    }
+  }
+  const jsonFile = JSON.stringify(list);
+  fs.writeFileSync(path, jsonFile, fixError)
+}
 
 
 (async ()=>{
+  let channel_name = [];
   const browser = await puppeteer.launch({ 
     headless: false,
     defaultViewport: false,
@@ -31,13 +41,22 @@ import puppeteer from 'puppeteer';
     } catch (error) {
         console.error(error)
     }
-    channels_list.push({channelName,videoUrl});
+    channels_list.push(videoUrl);
+    channel_name.push(channelName)
   }
-  console.table(channels_list)
+  const m3uVideoList = [...channels_list];
+  const m3uNameList = [...channel_name];
 
+  //save raw links
+      fs.writeFileSync('links.text',m3uVideoList.join("\n"))
+      console.log('saved raw links to links.text file')
+      fs.writeFileSync('names.text',m3uNameList.join("\n"))
+      console.log('saved raw links to names.text file')
+//file structure
+  const fileStructure = ["#EXTM3U",...m3uVideoList.map((link, i)=>`#EXTINF:-1 tvg-id="ext" group-title="ITALY",${m3uNameList[i]} \n ${link}`)].join("\n")
 
-
-
+//write m3u8 file
+  fs.writeFileSync('./italy_channels.m3u8', fileStructure)
 
 
 
