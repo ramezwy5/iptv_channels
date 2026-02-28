4b467be225f40ea5385a1880c8069f4317cfbbd2
ramezwy5 <ramez4fx@gmail.com>
2026-02-28 02:02:27 -0500

    azrogo scrape

--- arab_channels/arab_channels.js
+++ arab_channels/arab_channels.js
@@ -1,70 +1,71 @@
-import fs from 'node:fs';
-import puppeteer from 'puppeteer';
+import fs from "node:fs";
+import puppeteer from "puppeteer";
 
-function showsJson(path, list){
-  const fixError = (error)=>{
-    if(error){
-      console.error(error)
+function showsJson(path, list) {
+  const fixError = (error) => {
+    if (error) {
+      console.error(error);
       return;
     }
-  }
+  };
   const jsonFile = JSON.stringify(list);
-  fs.writeFileSync(path, jsonFile, fixError)
+  fs.writeFileSync(path, jsonFile, fixError);
 }
 
-
-(async ()=>{
-  let channel_name = [];
-  const browser = await puppeteer.launch({ 
-    headless: "new",
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      "--disable-extensions",
+      "--disable-features=HttpsFirstBalancedModeAutoEnable",
+    ],
     defaultViewport: false,
-    userDataDir: "./tmp", 
+    userDataDir: "./tmp",
   });
   const page = await browser.newPage();
-  const it_url = 'http://dagav.com/v/arabic/';
-  const channels_list = []
-  await page.goto(it_url, {waitUntil: 'networkidle2', timeout: 60000});
-  const videoLinkBar = await page.$$('.ChannelTV');
-  
+  const it_url = "http://azrogo.com/iphone/arabic/mobi_arabic_5.php";
+  const channels_list = [];
+  let channel_name = [];
+  await page.goto(it_url, { waitUntil: "networkidle2", timeout: 60000 });
+  const videoLinkBar = await page.$$(".BlockCha.Eg");
 
   //loop the elements
-  for(const bar of videoLinkBar){
+  for (const bar of videoLinkBar) {
     let videoUrl;
     let channelName;
     try {
-        videoUrl = await page.evaluate((el)=>el.querySelector(".channelPage").getAttribute("src"),bar)
+      videoUrl = await page.evaluate(
+        (el) => el.querySelector(".Azrotv-ChUrl").getAttribute("href"),
+        bar,
+      );
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
     try {
-        channelName = await page.evaluate((el)=>el.querySelector('.stream-title').innerText,bar)
+      channelName = await page.evaluate(
+        (el) => el.querySelector(".oui9img").alt,
+        bar,
+      );
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
-    channels_list.push(videoUrl);
-    channel_name.push(channelName)
+    channels_list.push(`http://azrogo.com${videoUrl}`);
+    channel_name.push(channelName);
   }
   const m3uVideoList = [...channels_list];
   const m3uNameList = [...channel_name];
-  console.log(m3uVideoList, m3uNameList)
+  console.log(m3uVideoList);
 
   //save raw links
-      fs.writeFileSync('links.text',m3uVideoList.join("\n"))
-      console.log('saved raw links to links.text file')
-      fs.writeFileSync('names.text',m3uNameList.join("\n"))
-      console.log('saved raw links to names.text file')
-//file structure
+  fs.writeFileSync("links.text", m3uVideoList.join("\n"));
+  console.log("saved raw links to links.text file");
+  fs.writeFileSync('names.text',m3uNameList.join("\n"))
+  console.log('saved raw links to names.text file')
+  //file structure
   //const fileStructure = ["#EXTM3U",...m3uVideoList.map((link, i)=>`#EXTINF:-1 tvg-id="ext" group-title="ITALY",${m3uNameList[i]} \n ${link}`)].join("\n")
 
-//write m3u8 file
+  //write m3u8 file
   //fs.writeFileSync('./italy_channels.m3u8', fileStructure)
-
-
-
-
-
-
-
 
   await browser.close();
 })();
