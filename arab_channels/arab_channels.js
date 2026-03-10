c20e3b1363cce6b083e3e0a50b2f7e8258ef7821
ramezwy5 <ramez4fx@gmail.com>
2026-03-10 17:14:34 -0400

    new list

--- arab_channels/arab_channels.js
+++ arab_channels/arab_channels.js
@@ -23,10 +23,10 @@ function showsJson(path, list) {
     userDataDir: "./tmp",
   });
   const page = await browser.newPage();
-  const it_url = "http://azrogo.com/iphone/arabic/mobi_arabic_5.php";
+  const country_url = "http://azrogo.com/iphone/arabic/mobi_arabic_5.php";
   const channels_list = [];
   let channel_name = [];
-  await page.goto(it_url, { waitUntil: "networkidle2", timeout: 60000 });
+  await page.goto(country_url, { waitUntil: "networkidle2" });
   const videoLinkBar = await page.$$(".BlockCha.Eg");
 
   //loop the elements
@@ -52,20 +52,41 @@ function showsJson(path, list) {
     channels_list.push(`http://azrogo.com${videoUrl}`);
     channel_name.push(channelName);
   }
-  const m3uVideoList = [...channels_list];
+  let links = [];
+  console.time('channels')
+  page.on("request", (request) => {
+    const url = request.url();
+    if (url.includes(".m3u8")) {
+      console.log("Found m3u8:", url);
+      links.push(url);
+    }
+  });
+  for (let channel of channels_list) {
+    await page.goto(channel, { waitUntil: "networkidle2", timeout: 0 });
+  }
+  console.timeEnd('channels')
+  console.log(links);
+  const m3uVideoList = [...links];
   const m3uNameList = [...channel_name];
-  console.log(m3uVideoList);
+  // console.log(m3uVideoList);
 
   //save raw links
   fs.writeFileSync("links.text", m3uVideoList.join("\n"));
   console.log("saved raw links to links.text file");
-  fs.writeFileSync('names.text',m3uNameList.join("\n"))
-  console.log('saved raw links to names.text file')
+  fs.writeFileSync("names.text", m3uNameList.join("\n"));
+  console.log("saved raw links to names.text file");
   //file structure
-  const fileStructure = ["#EXTM3U",...m3uVideoList.map((link, i)=>`#EXTINF:-1 tvg-id="ext" group-title="egypt",${m3uNameList[i]} \n ${link}`)].join("\n")
-
-  //write m3u8 file
-  fs.writeFileSync('./egypt_channels.m3u8', fileStructure)
+  if (m3uVideoList.length == m3uNameList.length) {
+    const fileStructure = [
+      "#EXTM3U",
+      ...m3uVideoList.map(
+        (link, i) =>
+          `#EXTINF:-1 tvg-id="ext" group-title="egypt",${m3uNameList[i]} \n ${link}`,
+      ),
+    ].join("\n");
+    //write m3u8 file
+    fs.writeFileSync("./egypt_channels.m3u8", fileStructure);
+  }
 
   await browser.close();
 })();
--- iptvExetract.js
+++ iptvExetract.js
@@ -1,34 +1,34 @@
-import  puppeteer from 'puppeteer';
-import fs from 'node:fs';
+import puppeteer from "puppeteer";
+import fs from "node:fs";
 
 (async () => {
-  const browser = await puppeteer.launch({ 
+  const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: false,
-    userDataDir: "./tmp", 
+    userDataDir: "./tmp",
   });
   const page = await browser.newPage();
-  let link = []
-  page.on('request', request => {
+  let link = [];
+  page.on("request", (request) => {
     const url = request.url();
-    if (url.includes('.m3u8')) {
-      console.log('Found m3u8:', url);
-      link.push(url)
+    if (url.includes(".m3u8")) {
+      console.log("Found m3u8:", url);
+      link.push(url);
     }
   });
-  await page.goto('http://azrogo.com/iphone/arabic/ch5/aloula_masriyah.php', {
-    waitUntil: 'networkidle2', timeout: 60000
+  await page.goto("http://azrogo.com/iphone/arabic/ch5/cbc.php", {
+    waitUntil: "networkidle2",
+    timeout: 60000,
   });
   const jsonFile = JSON.stringify(link);
-  console.log('Waiting for stream...');
-    // check file existing before append or create new file
-    fs.access('./links.json', fs.constants.F_OK,()=>{
-      if(true){
-        fs.appendFileSync('./links.json', jsonFile)
-      }
-    })
-    // fs.writeFileSync('./links.json', link)
-
+  console.log("Waiting for stream...");
+  // check file existing before append or create new file
+  fs.access("./links.json", fs.constants.F_OK, () => {
+    if (true) {
+      fs.appendFileSync("./links.json", jsonFile);
+    }
+  });
+  // fs.writeFileSync('./links.json', link)
 
   await browser.close();
 })();
